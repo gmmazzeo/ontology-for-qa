81c65cd91a8e59104e82b51fbfc296966cbda1e8
peterhuang <peterhuang913@gmail.com>
2015-04-14 00:13:05 -0700

    Refined mapOnOntology, attributeLookup, and added model numbers

--- src/main/java/edu/ucla/cs/scai/swim/qa/ontology/Ontology.java
+++ src/main/java/edu/ucla/cs/scai/swim/qa/ontology/Ontology.java
@@ -18,27 +18,29 @@ import java.util.Set;
 public interface Ontology {
     
     public ArrayList<? extends NamedEntityLookupResult> lookupEntity(String entityName) throws Exception;
-    
+
     public ArrayList<? extends AttributeLookupResult> lookupAttribute(String attributeName);
-    
+
     public ArrayList<? extends AttributeLookupResult> lookupAttribute(String attributeName, Set<String> subjectType, Set<String> valueType);
-    
+
+    public ArrayList<? extends AttributeLookupResult> lookupAttribute(String attributeName, Set<String> subjectType, Set<String> valueType, NamedEntity domain, NamedEntity range);
+
     public ArrayList<? extends AttributeLookupResult> lookupAttribute(String attributeName, Category subjectCategory, String range);
-    
+
     public ArrayList<? extends CategoryLookupResult> lookupCategory(String categoryName);
-    
+
     public ArrayList<? extends CategoryLookupResult> lookupCategory(Collection<Attribute> attributes);
-    
+
     public ArrayList<? extends NamedEntityAnnotationResult> annotateNamedEntities(String sentence);
-    
+
     public NamedEntity getEntityByUri(String uri);
-    
+
     public Category getCategoryByUri(String uri);
 
     public Attribute getAbstractAttribute();
-    
+
     public String getTypeAttribute();    
-    
+
     //public ArrayList<? extends QueryResult> executeQuery(Query q);
-    
+
 }
